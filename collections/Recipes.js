56a6d9e45aeed974b580987b07db7863ddaf0a03
Michael Vorster <michaelvorster@Michaels-MacBook-Pro.local>
2016-09-12 15:55:01 +0200

    Second commit

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -1,4 +1,24 @@
-Recipes = new Meteor.Collection('recipes');
+Recipes = new Mongo.Collection('recipes');
+
+Recipes.allow({
+	insert: function(userId, doc){
+		return !!userId;
+	},
+	update:  function(userId, doc){
+		return !!userId;
+	}
+});
+
+
+Ingredient = new SimpleSchema({
+	name: {
+		type: String
+	},
+	amount: {
+		type: String
+	}
+});
+
 
 RecipeSchema = new SimpleSchema({
 	name: {
@@ -8,6 +28,18 @@ RecipeSchema = new SimpleSchema({
 	description: {
 		type: String,
 		label: "Description"
+	},
+	ingredients: {
+		type: [Ingredient]
+
+	},
+	inMenu: {
+		type: Boolean,
+		defaultValue: false,
+		optional: true,
+		autoform: {
+			type: "hidden"
+		}
 	},
 	author: {
 		type: String,
@@ -30,5 +62,17 @@ RecipeSchema = new SimpleSchema({
 		}
 	}
 });
+
+
+Meteor.methods({
+	toggleMenuItem: function(id, currentState) {
+		Recipes.update(id, {
+			$set: {
+				inMenu: !currentState
+			}
+		});
+	}
+});
+
 
 Recipes.attachSchema(RecipeSchema);
